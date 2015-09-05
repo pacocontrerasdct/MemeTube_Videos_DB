d8b94a6845f35648c29e3f196c464f6b17efddf9
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-05 19:08:46 +0100

    add videos route and presenting in home route

--- app.rb
+++ app.rb
@@ -32,11 +32,54 @@ get '/views/styles.css' do
 end
 
 get '/new_tube' do
-  @title_of_page = "New tube"
+  #@title_of_page = "New tube"
 
-  
 
-  
+if params[:video_id]
+    
+    puts 'this is id: ',params[:video_id]
+    
+
+    id = params[:video_id]
+    title = params[:new_title]
+    description = params[:new_description]
+    genre = params[:new_genre]
+    url = params[:new_url]
+
+    sql = "UPDATE videos SET title = '#{title}', description = '#{description}', genre = '#{genre}', url = '#{url}'  WHERE id = '#{id}' "
+
+    new_video = @db.exec(sql)
+ 
+    puts 'update it' if new_video
+
+    redirect('/home')
+
+  elsif params[:new_title].to_s != ""
+    puts 'user has submitted the form'
+    puts 'this is tube: ',params[:new_tube]
+
+    title = params[:new_title]
+    description = params[:new_description]
+    genre = params[:new_genre]
+    url = params[:new_url]
+
+
+    sql = "insert into videos (title, description, genre, url) values ('#{title}', '#{description}', '#{genre}', '#{url}')"
+
+    new_tube = @db.exec(sql)
+ 
+    puts 'did it' if new_tube
+
+    redirect('/home')
+
+  end
+
+
+
+
+
+
+
   erb :new_tube
 end
 
