d3560e67001c2deaf799a99b780ab403062b01e1
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-06 01:55:56 +0100

    add new, update and delete videos, starting with finding a video

--- app.rb
+++ app.rb
@@ -11,7 +11,6 @@ after do
   @db.close
 end
 
-
 get '/' do
   @title_of_page = "index"
   redirect('/home')
@@ -34,11 +33,9 @@ end
 get '/new_tube' do
   #@title_of_page = "New tube"
 
-
-if params[:video_id]
+  if params[:video_id]
     
     puts 'this is id: ',params[:video_id]
-    
 
     id = params[:video_id]
     title = params[:new_title]
@@ -74,24 +71,48 @@ if params[:video_id]
 
   end
 
-
-
-
-
-
-
   erb :new_tube
 end
 
 get '/find_tube' do
-  @title_of_page = "find tube"
+  
+  if params[:new_title]
+    title = params[:new_title]
+    sql = "select * from videos where title LIKE '%#{title}%' "
+    @find_video = @db.exec(sql)
+    erb :find_tube
+  else
+    @find_no_video = "No videos on site with that query"
+  end
+
+  #if params[:new_genre]
+  #  genre = params[:new_genre]
+  #  sql = "select * from videos where genre LIKE '%#{genre}%' "
+  #  @find_video = @db.exec(sql)
+  #  erb :find_tube
+  #else
+  #  @find_no_video = "No videos on site with that query"
+  #end
+
   erb :find_tube
+  
 end
 
 get '/update_tube' do
-  
+  sql = "SELECT * FROM videos WHERE id = #{params[:id]} LIMIT 1"
+  video_row = @db.exec(sql)
+  @updated_title = video_row.first['title']
+  @updated_description = video_row.first['description']
+  @updated_genre = video_row.first['genre']
+  @updated_url = video_row.first['url']
+  @video_id = params[:id]
+  erb :update_tube
 end
 
 get '/delete_tube' do
-  
+  id = params[:id]
+  sql = "DELETE FROM videos WHERE id = '#{id}' "
+  @db.exec(sql)
+
+  redirect('/home')
 end
