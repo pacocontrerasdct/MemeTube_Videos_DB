f2bbbe1d1906ffe5f16e96acfc0dabdf2e1cb5f8
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-06 12:14:26 +0100

    got every matching word looking into title, genre or description but have got duplicates

--- app.rb
+++ app.rb
@@ -75,57 +75,62 @@ get '/new_tube' do
 end
 
 get '/find_tube' do
-  
+  # to the next array i'll pass id from videos that match word
+  @found_videos = []
+
+  @found_videos << 1
+
   if params[:find_me].to_s != ""
     @find_me = params[:find_me].downcase
-    puts "find me this:", @find_me
-    
+    #puts "find me this:", @find_me 
     finding_fields = ['title', 'genre', 'description']   
     
+    @found_videos << 2
+
     finding_fields.each { |find_in| 
+      #puts "find_me:", @find_me
+      #puts "find_in:", find_in
       
-      puts "find_me:", @find_me
 
-      puts "find_in:", find_in
+      # Getting all matching rows
+      sql = "select count(id) from videos where #{find_in} LIKE '%#{@find_me}%' "
+      count = @db.exec(sql)
+      num_match_results = count.first['count'].to_i
+      
+      # Next select every single row and push it towards @found_videos array
+      
+        
+        # Next select a single result
+        sql = "select * from videos where #{find_in} LIKE '%#{@find_me}%' "
+        result = @db.exec(sql)
+        puts result.to_a
+        puts "sql inside upto:", sql
+      
+      
+
+      
+      
+      
 
 
-      sql = "select * from videos where #{find_in} LIKE '%#{@find_me}%' "
-      @find_video = @db.exec(sql)
-      puts "sql:", sql
-      if @find_video.first
-        found = @find_video.first
-        puts "found:", found
-        erb :find_tube
-      else
-        puts "not found"
-      end
+
+
+
+      #if @find_video.first
+      #  found = @find_video.first
+      #  puts "found:", found['id']
+      #  puts "found:", found
+      #  @found_videos << found['id']
+      #  erb :find_tube
+      #end
     }
+    @found_videos << 3
+    puts "next is array:" , @found_videos
 
   end 
 
 
- #   title = title.downcase
- #   genre = genre.downcase
- #   description = description.downcase
 
- #   finding_fields = [title, genre, description]
-
- #   finding_fields.each { |find_in| 
- #       sql = "select * from videos where title LIKE '%#{find_in}%' "
- #   } 
-
- #   sql = "select * from videos where title LIKE '%#{title}%' "
- #   sql = "select * from videos where genre LIKE '%#{genre}%' "
- #   sql = "select * from videos where description LIKE '%#{description}%' "
- #   
-
-
-
-
- #   @find_video = @db.exec(sql)
- #   
-
- #   
  #   if @find_video.first
  #     erb :find_tube 
  #   else
